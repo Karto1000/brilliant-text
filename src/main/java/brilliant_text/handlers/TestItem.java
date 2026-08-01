02cbd8c71303f55bb921abf577b97150452c5346
Karto1000 <tim.leuenberger@bluewin.ch>
2026-08-01 15:31:42 +0200

    Allow brilliant formatting codes to be applied after the beginning of a String Fix error with authorList in mcmod.info

--- src/main/java/brilliant_text/handlers/TestItem.java
+++ src/main/java/brilliant_text/handlers/TestItem.java
@@ -1,32 +1,46 @@
 package brilliant_text.handlers;
 
 import brilliant_text.BrilliantText;
-import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.common.IRarity;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class TestItem extends Item {
     private final EnumRarity rarity;
+    private final String registryId;
 
     public TestItem(String id, EnumRarity rarity) {
-        this.setRegistryName(BrilliantText.MODID,id);
+        this.setRegistryName(BrilliantText.MODID, id);
         this.setTranslationKey(String.format("brilliant_text.%s", id));
+        this.registryId = id;
         this.rarity = rarity;
     }
 
     public TestItem(String id) {
-        this.setRegistryName(BrilliantText.MODID,id);
+        this.setRegistryName(BrilliantText.MODID, id);
         this.setTranslationKey(String.format("brilliant_text.%s", id));
+        this.registryId = id;
         this.rarity = EnumRarity.COMMON;
     }
 
     @Override
+    @Nonnull
     public IRarity getForgeRarity(@Nonnull ItemStack stack) {
         return this.rarity;
+    }
+
+    @Override
+    public void addInformation(@Nonnull ItemStack stack, @Nullable World worldIn, @Nonnull List<String> tooltip, @Nonnull ITooltipFlag flagIn) {
+        if (!I18n.hasKey(String.format("tooltip.brilliant_text.%s", registryId))) return;
+        tooltip.add(I18n.format(String.format("tooltip.brilliant_text.%s", registryId)));
     }
 }
 
--- src/main/java/brilliant_text/mixin/vanilla/FontRendererMixin.java
+++ src/main/java/brilliant_text/mixin/vanilla/FontRendererMixin.java
@@ -1,8 +1,8 @@
 package brilliant_text.mixin.vanilla;
 
 import brilliant_text.BrilliantText;
-import brilliant_text.shader.BrilliantTextRenderer;
 import brilliant_text.shader.BrilliantTextManager;
+import brilliant_text.shader.BrilliantTextRenderer;
 import brilliant_text.shader.FormatCharacter;
 import brilliant_text.shader.ITextShader;
 import brilliant_text.util.AABB2D;
@@ -10,135 +10,272 @@ import brilliant_text.util.TextureTargetManager;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Locale;
 import java.util.Optional;
+import java.util.Random;
 
 @SideOnly(Side.CLIENT)
 @Mixin(FontRenderer.class)
 public abstract class FontRendererMixin {
-    @Shadow
-    public abstract int getStringWidth(String text);
 
     @Shadow
     public int FONT_HEIGHT;
 
-    @ModifyVariable(
-            method = "drawString(Ljava/lang/String;FFIZ)I",
-            at = @At("HEAD"),
-            ordinal = 0,
-            argsOnly = true
-    )
-    public boolean brilliant_text_drawString(
-            boolean dropShadow,
-            String text,
-            float x,
-            float y,
-            int color
-    ) {
-        if (BrilliantTextRenderer.getFboTarget() == null) return dropShadow;
-        if (text.trim().length() < 2) return dropShadow;
+    @Shadow
+    private boolean randomStyle;
 
-        String strippedText = text.trim().replaceAll("(?i)§[0-9A-FK-OR]", "");
-        if (strippedText.length() < 2) return dropShadow;
+    @Shadow
+    private boolean boldStyle;
 
-        String firstTwo = strippedText.substring(0, 2);
-        if (!firstTwo.startsWith(String.valueOf(BrilliantTextManager.FORMAT_CHARACTER_PREFIX))) return dropShadow;
+    @Shadow
+    private boolean strikethroughStyle;
 
-        char formatChar = firstTwo.charAt(1);
-        if (FormatCharacter.isInvalidCharacter(formatChar)) return dropShadow;
+    @Shadow
+    private boolean underlineStyle;
 
-        try {
-            FormatCharacter formatCharacter = FormatCharacter.tryFrom(formatChar);
+    @Shadow
+    private boolean italicStyle;
 
-            Optional<ITextShader> oShader = BrilliantTextManager.getShader(formatCharacter);
-            if (!oShader.isPresent()) {
-                BrilliantText.LOGGER.warn("No Shader for format character {}", formatCharacter);
-                return dropShadow;
-            }
+    @Unique
+    private boolean brilliantText$brilliantStyle;
 
-            return false;
+    @Unique
+    private ITextShader brilliantText$currentShader;
 
-        } catch (IllegalArgumentException ignored) {
-            BrilliantText.LOGGER.warn("Character {} is not allowed", formatChar);
-        }
+    @Unique
+    private float brilliantText$brilliantTextStartX;
 
-        return dropShadow;
-    }
+    @Unique
+    private float brilliantText$brilliantTextStartY;
 
-    @Inject(method = "drawString(Ljava/lang/String;FFIZ)I", at = @At("HEAD"))
-    public void brilliant_text_drawString(
-            String text,
-            float x,
-            float y,
-            int color,
-            boolean dropShadow,
-            CallbackInfoReturnable<Integer> cir
-    ) {
-        if (BrilliantTextRenderer.getFboTarget() == null) return;
-        if (text.trim().length() < 2) return;
+    @Unique
+    private float brilliantText$brilliantTextWidth;
 
-        String strippedText = text.trim().replaceAll("(?i)§[0-9A-FK-OR]", "");
-        if (strippedText.length() < 2) return;
+    @Shadow
+    @Final
+    private int[] colorCode;
 
-        String firstTwo = strippedText.substring(0, 2);
-        if (!firstTwo.startsWith(String.valueOf(BrilliantTextManager.FORMAT_CHARACTER_PREFIX))) return;
+    @Shadow
+    private int textColor;
 
-        char formatChar = firstTwo.charAt(1);
-        if (FormatCharacter.isInvalidCharacter(formatChar)) return;
+    @Shadow
+    protected abstract void setColor(float r, float g, float b, float a);
 
-        try {
-            FormatCharacter formatCharacter = FormatCharacter.tryFrom(formatChar);
+    @Shadow
+    private float red;
 
-            Optional<ITextShader> oShader = BrilliantTextManager.getShader(formatCharacter);
-            if (!oShader.isPresent()) {
-                BrilliantText.LOGGER.warn("No Shader for format character {}", formatCharacter);
-                return;
-            }
+    @Shadow
+    private float blue;
 
-            ITextShader shader = oShader.get();
+    @Shadow
+    private float green;
 
+    @Shadow
+    private float alpha;
+
+    @Shadow
+    public abstract int getCharWidth(char character);
+
+    @Shadow
+    private boolean unicodeFlag;
+
+    @Shadow
+    protected float posX;
+
+    @Shadow
+    protected float posY;
+
+    @Shadow
+    protected abstract float renderChar(char ch, boolean italic);
+
+    @Shadow
+    protected abstract void doDraw(float f);
+
+    @Shadow
+    public Random fontRandom;
+
+    @Unique
+    private void brilliantText$resetBrilliantStyle() {
+        assert brilliantText$currentShader != null;
+
+        if (this.brilliantText$brilliantStyle) {
             BrilliantTextRenderer.addBrilliantTextAt(
-                    new AABB2D(x, y, this.getStringWidth(text), this.FONT_HEIGHT),
-                    shader
+                    new AABB2D(
+                            this.brilliantText$brilliantTextStartX,
+                            this.brilliantText$brilliantTextStartY,
+                            (int) this.brilliantText$brilliantTextWidth,
+                            this.FONT_HEIGHT
+                    ),
+                    brilliantText$currentShader
             );
-            TextureTargetManager.bindFBO(BrilliantTextRenderer.getFboTarget());
-        } catch (IllegalArgumentException ignored) {
-            BrilliantText.LOGGER.warn("Character {} is not allowed", formatChar);
         }
+
+        this.brilliantText$brilliantTextStartX = 0;
+        this.brilliantText$brilliantTextStartY = 0;
+        this.brilliantText$brilliantTextWidth = 0;
+        this.brilliantText$currentShader = null;
+        this.brilliantText$brilliantStyle = false;
     }
 
-    @Inject(method = "drawString(Ljava/lang/String;FFIZ)I", at = @At("TAIL"))
-    public void brilliant_text_drawStringTail(
-            String text,
-            float x,
-            float y,
-            int color,
-            boolean dropShadow,
-            CallbackInfoReturnable<Integer> cir
-    ) {
-        TextureTargetManager.bindScreen();
-    }
+    @Inject(method = "renderStringAtPos", at = @At("HEAD"), cancellable = true)
+    public void brilliant_text_renderStringAtPosPatch(String text, boolean shadow, CallbackInfo ci) {
+        for (int i = 0; i < text.length(); ++i) {
+            char currentChar = text.charAt(i);
 
-    @Redirect(
-            method = "renderStringAtPos",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/lang/String;indexOf(I)I"
-            )
-    )
-    private int redirectFormatIndexOf(String formatCodes, int ch) {
-        int index = formatCodes.indexOf(ch);
+            if (currentChar == '§' && i + 1 < text.length()) {
+                char nextChar = text.charAt(i + 1);
 
-        // If it's a custom code (index == -1), return 99 (> 21) so it bypasses
-        // the `if (i1 < 16)` check that resets styles and forces white color (i1 = 15)
-        if (index == -1) return 99;
-        return index;
+                if (!brilliantText$brilliantStyle && !FormatCharacter.isInvalidCharacter(nextChar)) {
+                    // We don't want to draw the shadow when the formatting is brilliant
+                    if (BrilliantTextRenderer.getFboTarget() == null || shadow) {
+                        i++;
+                        continue;
+                    }
+
+                    FormatCharacter formatCharacter = FormatCharacter.tryFrom(nextChar);
+
+                    Optional<ITextShader> oShader = BrilliantTextManager.getShader(formatCharacter);
+                    if (oShader.isPresent()) {
+                        ITextShader shader = oShader.get();
+
+                        this.brilliantText$brilliantStyle = true;
+                        this.brilliantText$currentShader = shader;
+                        this.brilliantText$brilliantTextStartX = this.posX;
+                        this.brilliantText$brilliantTextStartY = this.posY;
+
+                        TextureTargetManager.bindFBO(BrilliantTextRenderer.getFboTarget());
+                        i++;
+                        continue;
+                    }
+
+                    BrilliantText.LOGGER.warn("No bound shader for character {}", nextChar);
+                    i++;
+                    continue;
+                }
+
+                int formatIndex = "0123456789abcdefklmnor".indexOf(
+                        String.valueOf(nextChar)
+                                .toLowerCase(Locale.ROOT)
+                                .charAt(0)
+                );
+
+                if (formatIndex < 16) {
+                    this.randomStyle = false;
+                    this.boldStyle = false;
+                    this.strikethroughStyle = false;
+                    this.underlineStyle = false;
+                    this.italicStyle = false;
+
+                    this.brilliantText$resetBrilliantStyle();
+                    TextureTargetManager.bindScreen();
+
+                    if (formatIndex < 0) formatIndex = 15;
+                    if (shadow) formatIndex += 16;
+
+                    int j1 = this.colorCode[formatIndex];
+                    this.textColor = j1;
+                    setColor(
+                            (float) (j1 >> 16) / 255.0F,
+                            (float) (j1 >> 8 & 255) / 255.0F,
+                            (float) (j1 & 255) / 255.0F,
+                            this.alpha
+                    );
+                } else if (formatIndex == 16) {
+                    this.randomStyle = true;
+                } else if (formatIndex == 17) {
+                    this.boldStyle = true;
+                } else if (formatIndex == 18) {
+                    this.strikethroughStyle = true;
+                } else if (formatIndex == 19) {
+                    this.underlineStyle = true;
+                } else if (formatIndex == 20) {
+                    this.italicStyle = true;
+                } else {
+                    this.randomStyle = false;
+                    this.boldStyle = false;
+                    this.strikethroughStyle = false;
+                    this.underlineStyle = false;
+                    this.italicStyle = false;
+
+                    this.brilliantText$resetBrilliantStyle();
+                    TextureTargetManager.bindScreen();
+
+                    setColor(this.red, this.blue, this.green, this.alpha);
+                }
+
+                i++;
+                continue;
+            }
+
+            int j = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".indexOf(
+                    currentChar);
+
+            if (this.randomStyle && j != -1) {
+                int k = this.getCharWidth(currentChar);
+                char c1;
+
+                do {
+                    j = this.fontRandom.nextInt(
+                            "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".length());
+                    c1 = "\u00c0\u00c1\u00c2\u00c8\u00ca\u00cb\u00cd\u00d3\u00d4\u00d5\u00da\u00df\u00e3\u00f5\u011f\u0130\u0131\u0152\u0153\u015e\u015f\u0174\u0175\u017e\u0207\u0000\u0000\u0000\u0000\u0000\u0000\u0000 !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\u0000\u00c7\u00fc\u00e9\u00e2\u00e4\u00e0\u00e5\u00e7\u00ea\u00eb\u00e8\u00ef\u00ee\u00ec\u00c4\u00c5\u00c9\u00e6\u00c6\u00f4\u00f6\u00f2\u00fb\u00f9\u00ff\u00d6\u00dc\u00f8\u00a3\u00d8\u00d7\u0192\u00e1\u00ed\u00f3\u00fa\u00f1\u00d1\u00aa\u00ba\u00bf\u00ae\u00ac\u00bd\u00bc\u00a1\u00ab\u00bb\u2591\u2592\u2593\u2502\u2524\u2561\u2562\u2556\u2555\u2563\u2551\u2557\u255d\u255c\u255b\u2510\u2514\u2534\u252c\u251c\u2500\u253c\u255e\u255f\u255a\u2554\u2569\u2566\u2560\u2550\u256c\u2567\u2568\u2564\u2565\u2559\u2558\u2552\u2553\u256b\u256a\u2518\u250c\u2588\u2584\u258c\u2590\u2580\u03b1\u03b2\u0393\u03c0\u03a3\u03c3\u03bc\u03c4\u03a6\u0398\u03a9\u03b4\u221e\u2205\u2208\u2229\u2261\u00b1\u2265\u2264\u2320\u2321\u00f7\u2248\u00b0\u2219\u00b7\u221a\u207f\u00b2\u25a0\u0000".charAt(
+                            j);
+
+                } while (k != this.getCharWidth(c1));
+
+                currentChar = c1;
+            }
+
+            float f1 = j == -1 || this.unicodeFlag ? 0.5f : 1f;
+            boolean flag = (currentChar == 0 || j == -1 || this.unicodeFlag) && shadow;
+
+            if (flag) {
+                this.posX -= f1;
+                this.posY -= f1;
+            }
+
+            float characterWidth = this.renderChar(currentChar, this.italicStyle);
+
+            if (flag) {
+                this.posX += f1;
+                this.posY += f1;
+            }
+
+            if (this.boldStyle) {
+                this.posX += f1;
+
+                if (flag) {
+                    this.posX -= f1;
+                    this.posY -= f1;
+                }
+
+                this.renderChar(currentChar, this.italicStyle);
+                this.posX -= f1;
+
+                if (flag) {
+                    this.posX += f1;
+                    this.posY += f1;
+                }
+
+                characterWidth++;
+            }
+
+            if (this.brilliantText$brilliantStyle) this.brilliantText$brilliantTextWidth += characterWidth;
+            this.doDraw(characterWidth);
+        }
+
+        if (brilliantText$brilliantStyle) {
+            this.brilliantText$resetBrilliantStyle();
+            TextureTargetManager.bindScreen();
+        }
+
+        ci.cancel();
     }
 }
